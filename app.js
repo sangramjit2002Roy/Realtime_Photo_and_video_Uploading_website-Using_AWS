960cd5f503a8f326986f55e8e44cdeeead2ec99d
sangramjit2002Roy <sangram1232002@gmail.com>
2023-01-04 13:52:00 +0530

    delete request for s3 and mongodb done

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ import {
   S3Client,
   PutObjectCommand,
   GetObjectCommand,
+  DeleteObjectCommand,
 } from "@aws-sdk/client-s3";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 
@@ -48,22 +49,22 @@ app.get("/api/posts", async (req, res) => {
   // < Generate URL's From the server That will allow users to see the images for a temporary ammount of time >
   // summary: Generating Signed URL (Allowing access temporaryly to the image)
   const posts = await prisma.posts.findMany({ orderBy: [{ created: "desc" }] });
-  for(const post of posts){
+  for (const post of posts) {
     const getObjectParams = {
       Bucket: bucketName,
       Key: post.imageName,
-    }
+    };
     const command = new GetObjectCommand(getObjectParams);
     const url = await getSignedUrl(s3, command, { expiresIn: 3600 }); //3600 second = 1 hour
-    post.imageUrl = url
+    post.imageUrl = url;
     // </ Generate URL's From the server That will allow users to see the images for a temporary ammount of time >
   }
 
   res.send(posts);
 });
 app.post("/api/posts", upload.single("image"), async (req, res) => {
-  console.log(req.body);
-  console.log(req.file);
+  // console.log(req.body);
+  // console.log(req.file);
   try {
     // resizing image
     const buffer = await sharp(req.file.buffer)
@@ -89,17 +90,103 @@ app.post("/api/posts", upload.single("image"), async (req, res) => {
     });
     // </ SAVING_THE_DATA_TO_THE_DATABASE_(mongoDB) >
 
-
-
-    res.send(post);
+    // res.send(post);
+    res.send(`
+    <html>
+  <head>
+    <link href="https://fonts.googleapis.com/css?family=Nunito+Sans:400,400i,700,900&display=swap" rel="stylesheet">
+  </head>
+    <style>
+      body {
+        text-align: center;
+        padding: 40px 0;
+        background: #EBF0F5;
+      }
+        h1 {
+          color: #88B04B;
+          font-family: "Nunito Sans", "Helvetica Neue", sans-serif;
+          font-weight: 900;
+          font-size: 40px;
+          margin-bottom: 10px;
+        }
+        p {
+          color: #404F5E;
+          font-family: "Nunito Sans", "Helvetica Neue", sans-serif;
+          font-size:20px;
+          margin: 0;
+        }
+      i {
+        color: #9ABC66;
+        font-size: 100px;
+        line-height: 200px;
+        margin-left:-15px;
+      }
+      .card {
+        background: white;
+        padding: 60px;
+        border-radius: 4px;
+        box-shadow: 0 2px 3px #C8D0D8;
+        display: inline-block;
+        margin: 0 auto;
+      }
+    </style>
+    <body>
+      <div class="card">
+      <div style="border-radius:200px; height:200px; width:200px; background: #F8FAF5; margin:0 auto;">
+        <i class="checkmark">✓</i>
+      </div>
+        <h1>Success</h1> 
+        <p>Post Request is Successfull;<br/><a class="rli" href="/index.html">Home</a></p>
+      </div>
+    </body>
+</html>
+    `);
   } catch (error) {
     console.log(error);
     res.status(404).send(`msg: ${error}`);
   }
 });
+/*
+// In the CASE of REACT frontend we can use this becoz plain HTML-form doesn't support DELETE request
 app.delete("/api/posts/:id", async (req, res) => {
-  const id = req.params.id;
-  res.send({});
-});
+  const id = +req.params.id;
+  const post = await prisma.posts.findUnique({ where: { id } });
+  if (!post) {
+    res.status(404).send("Post not found");
+    return;
+  }
+  const params = {
+    Bucket: bucketName,
+    Key: post.imageName,
+  };
 
+  const command = new DeleteObjectCommand(params);
+  await s3.send(command);
+
+  await prisma.posts.delete({ where: { id } });
+
+  res.send(post);
+});
+*/
+app.post("/api/deletePost/:id", async (req, res) => {
+  const id = req.params.id;
+  // console.log(id);
+  const post = await prisma.posts.findUnique({ where: { id: id } });
+  if (!post) {
+    res.status(404).send("Post not found");
+    return;
+  }
+
+  const params = {
+    Bucket: bucketName,
+    Key: post.imageName,
+  };
+  const command = new DeleteObjectCommand(params);
+
+  await s3.send(command);
+
+  // await deleteFile(post.imageName);
+  await prisma.posts.delete({ where: { id } });
+  res.redirect("/");
+});
 app.listen(3000, console.log(`server is running on port 3000...`));
